92625ad3baa8d8a6322931691bd642d0a24cf70b
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-01 09:23:46 -0400

    change timer, remove jquery

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 
-$("h1").click(function(){
+var timerElement = document.getElementsByClassName("timer_start");
+console.log(timerElement);
+timerElement.addEventListener("click", timerBeginCount);
+function timerBeginCount(){
+//Set Time + 5 mins
+let countDownDate = new Date().getTime();
+countDownDate = countDownDate + 300000;
 // Update the count down every 1 second
-var x = setInterval(function() {
-  // Get todays date and time
-  var countDownDate = new Date().getTime();
-  countDownDate = countDownDate + 300000;
-  var now = new Date().getTime();
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-  // Time calculations for days, hours, minutes and seconds
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("clockdiv").innerHTML = minutes + "m " + seconds + "s ";
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("clockdiv").innerHTML = "EXPIRED";
-  }
-}, 1000);
-});
+  var x = setInterval(function() {
+    let now = new Date().getTime();
+    // Find the distance between now an the count down date
+    let distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("clockdiv").innerHTML = minutes + "m " + seconds + "s ";
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("clockdiv").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+};
