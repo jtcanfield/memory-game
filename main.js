221245bec7b9d248d5a6c5af34ccd932ee611431
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-04 11:43:09 -0400

    add functions for updating sliders

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let cardsfinished = 0;
 let cardsRequested = 0; // default 50
 let timeSelected = 0; //default 300000
 let healthSelected = 0; //default 300000
+let timeSelectedCaluculated = 0;
 let gameCardsObject = {//opens parenting object
   "results":[//opens array
     /*
@@ -82,42 +83,45 @@ function checkName(){
 //BEGIN OPTIONS MENU
 function createOptionsMenu(userName){
     let holder = `
-    <div id="pre_game_body">
-      <div id="pre_game_inner_body">
+    <div id="pre_game_options_menu">
+      <div id="pre_game_inner_options_menu">
         <h2>Select your options, ${userName}.</h2>
-        <input type="range" id="CardSelection" step="2" min="40" max="70" value="50" onchange="updateCards(this.value)" oninput="updateCards(this.value)"></input><br>
+        <input type="range" id="cardSelection" step="2" min="40" max="70" value="50" onchange="updateCards(this.value)" oninput="updateCards(this.value)"></input><br>
         <p>Number of Cards: <span id="cardsInput">50</span></p>
         <input type="range" id="healthSelection" step="2" min="40" max="70" value="50" onchange="updateHealth(this.value)" oninput="updateHealth(this.value)"></input><br>
         <p>Health: <span id="healthInput">50</span></p>
         <input type="range" id="timeSelection" step="15000" min="180000" max="375000" value="300000" onchange="updateTime(this.value)" oninput="updateTime(this.value)"></input><br>
         <p>Health: <span id="timeInput">50</span></p>
-        <button onclick="detectAmount()">Try it</button>
+        <button onclick="nextSection()">Try it</button>
       </div>
     </div>
     `;
     if (360000) {}
   htmlBody.innerHTML = holder;
   fadeIn(document.getElementById("pre_game_body"), .01);
-  timeSelected
-  timeSelectedCaluculated
-  // setTimeout(function() { createPreGame(userName, timeSelectedCaluculated);}, 2000);
+  //BEGIN OPTIONS DETECTION
+  function updateCards(val) {
+    document.getElementById('cardsInput').innerHTML=val;
+    cardsRequested = document.getElementById("cardSelection").value;
+  }
+  function updateHealth(val) {
+    document.getElementById('healthInput').innerHTML=val;
+  }
+  function updateTime(val) {
+    timeSelected = document.getElementById("timeSelection").value;
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    timeSelectedCaluculated = minutes + "minutes and " + seconds + "seconds ";
+    document.getElementById('timeInput').innerHTML=val;
+  }
+  //END OPTIONS DETECTION
+  function nextSection(){
+    fadeOut(document.getElementById("name_entry"), .01);
+    console.log(cardsRequested);
+    // setTimeout(function() { createPreGame(userName, timeSelectedCaluculated);}, 2000);
+  }
 }
-//BEGIN CARD AMOUNT CHOICE
-function detectAmount(){
-  //Add Time Change
-  //Add Health Change
-
-  cardsRequested = document.getElementById("CardSelection").value;
-  console.log(cardsRequested);
-}
-function updateCards(val) {
-  document.getElementById('cardsInput').innerHTML=val;
-}
-function updateHealth(val) {
-  document.getElementById('healthInput').innerHTML=val;
-}
-//END CARD AMOUNT CHOICE
-//OPTIONS MENU
+//END OPTIONS MENU
 
 //BEGIN INTRO
 function createPreGame(userName, time){
